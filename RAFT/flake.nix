c52c9a64d6fb978a66ffade711902b7b91b2fda7
Flexksx <cristian.cretu2808@gmail.com>
2024-12-04 18:44:05 +0200

    add flake for dev

---
+++ RAFT/flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "OneDollarBid Development Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.python312
+            pkgs.docker
+            pkgs.docker-compose
+          ];
+        };
+      }
+    );
+}
